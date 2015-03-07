b144e51ebb35db33c68b000baef1e528b2c23d06
edwin.webb <edwin.webb@gmail.com>
2015-03-07 11:49:22 +0000

    Add initial package and webpack config

--- README.md
+++ README.md
@@ -15,3 +15,52 @@ rm -rf .git
 ## npm scripts
 
 * `npm start` - Build and start the project in dev mode at http://localhost:8000
+
+
+## npm scripts
+
+* `npm start` - Build and start the app in dev mode at http://localhost:8000
+* `npm run build` - Run a production build
+
+
+## webpack
+
+`import` Files from within your JavaScript component files:
+
+```js
+// Filename: app.jsx
+import 'assets/images/logo.png';
+```
+
+## HTML files
+
+All required `.html` files are compiled with lodash.template and synced into the `./build` directory:
+
+```js
+// Filename: app.jsx
+import './index.html';
+```
+
+* You can adjust the lodash template data in the `webpack.config.js` file.
+
+## Releasing
+
+Updating version:
+
+* `npm version patch` - Bump version
+* `git push && git push --tags` - Push to remote
+
+Publishing package:
+
+* `npm login` - Login to npm
+* `npm publish` - Publish package
+
+## Credits
+
+This project was initially forked from https://github.com/badsyntax/react-seed
+
+## License
+
+Copyright (c) 2015 Edwin Webb
+
+MIT (http://opensource.org/licenses/MIT)
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+var path = require('path');
+var util = require('util');
+var webpack = require('webpack');
+var pkg = require('./package.json');
+
+var DEBUG = process.env.NODE_ENV !== 'production';
+
+var jsBundle = path.join('js', util.format('[name].%s.js', pkg.version));
+
+var plugins =[
+  new webpack.optimize.OccurenceOrderPlugin()
+];
+
+if (DEBUG) {
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  );
+} else {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NoErrorsPlugin()
+  );
+}
+
+var loaders = [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader?optional&optional=runtime'
+  },
+  {
+    test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/,
+    loader: 'file-loader?name=[path][name].[ext]'
+  },
+  {
+    test: /\.html$/,
+    loader: [
+      'file-loader?name=[path][name].[ext]',
+      'template-html-loader?' + [
+        'raw=true',
+        'engine=lodash',
+        'version='+pkg.version
+      ].join('&')
+    ].join('!')
+  }
+];
+
+var config = {
+  context: path.join(__dirname, 'app'),
+  cache: DEBUG,
+  debug: DEBUG,
+  target: 'web',
+  devtool: DEBUG ? '#inline-source-map' : false,
+  entry: {
+    app: [
+      'webpack/hot/dev-server',
+      './app.jsx'
+    ]
+  },
+  output: {
+    path: pkg.config.build_dir,
+    publicPath: '/',
+    filename: jsBundle,
+    pathinfo: DEBUG
+  },
+  module: {
+    loaders: loaders
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx']
+  }
+};
+
+module.exports = config;
