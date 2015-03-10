658fbc1c0f765fbb186a00caabf6a43539207f43
edwin.webb <edwin.webb@gmail.com>
2015-03-10 22:44:23 +0000

    rename map

--- app/displayobjects/Background/Background.jsx
+++ app/displayobjects/Background/Background.jsx
@@ -2,17 +2,19 @@
 
 import PIXI from 'pixi.js';
 import './texture.jpg';
-import './map@2x.jpg';
+import './map.jpg';
 
 export default class Background extends PIXI.DisplayObjectContainer {
 
   constructor() {
     super();
-    
-    var map = PIXI.Texture.fromImage('././displayobjects/Background/map@2x.jpg');
+
+    var map = PIXI.Texture.fromImage('././displayobjects/Background/map.jpg');
     var bg = PIXI.Sprite.fromImage('././displayobjects/Background/texture.jpg');
-    //var filter = new PIXI.NormalMapFilter(map); // Undefined?
-    //bg.filters = [filter];
+    // var filter = new PIXI.NormalMapFilter(map); // Undefined?
+    // var filter = new PIXI.DotScreenFilter();
+    //     filter.scale = 4;
+    bg.filters = [filter];
 
     this.addChild(bg);
   }
