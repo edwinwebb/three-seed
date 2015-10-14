1c1c46214bb9be71fd6e355783e50e78b49f492a
Edwin Webb <edwin.webb@razorfish.com>
2015-10-14 16:07:44 +0100

    Add a bunny

--- app/app.js
+++ app/app.js
@@ -11,14 +11,16 @@ import {config} from '../package.json';
 import Renderer from './Renderer/Renderer';
 import AnimationStore from './stores/AnimationStore';
 import {Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh} from 'three';
+import Bunny from './Bunny/Bunny';
 
 const renderer = new Renderer(config.stageWidth, config.stageHeight);
-
 const scene = new Scene();
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+
 const geometry = new BoxGeometry( 200, 200, 200 );
 const material = new MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-const mesh = new Mesh( geometry, material );
+const bunny = new Bunny();
+const mesh = new Mesh( bunny.geometry, material );
 
 scene.add(mesh);
 camera.position.z = 1000;
@@ -27,8 +29,7 @@ renderer.camera = camera;
 renderer.scene = scene;
 
 AnimationStore.addChangeListener(function() {
-  mesh.rotation.x += 0.1;
-  mesh.rotation.y += 0.1;
+  //mesh.rotation.z += 0.1;
 });
 
 renderer.start();
