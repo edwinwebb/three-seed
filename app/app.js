d1f700e2b88dd0ad30036395bafc5db2ea6993d0
Edwin Webb <edwin.webb@razorfish.com>
2015-10-14 16:17:16 +0100

    Add an orbit camera and rotate bunny

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ import {config} from '../package.json';
 import Renderer from './Renderer/Renderer';
 import AnimationStore from './stores/AnimationStore';
 import {Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh} from 'three';
+import THREE from 'three';
 import Bunny from './Bunny/Bunny';
 
 const renderer = new Renderer(config.stageWidth, config.stageHeight);
@@ -21,12 +22,16 @@ const geometry = new BoxGeometry( 200, 200, 200 );
 const material = new MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 const bunny = new Bunny();
 const mesh = new Mesh( bunny.geometry, material );
+const OrbitControls = require('three-orbit-controls')(THREE)
+const controls = new OrbitControls(camera);
 
 scene.add(mesh);
 camera.position.z = 1000;
 
 renderer.camera = camera;
 renderer.scene = scene;
+
+mesh.rotation.x = -90;
 
 AnimationStore.addChangeListener(function() {
   //mesh.rotation.z += 0.1;
