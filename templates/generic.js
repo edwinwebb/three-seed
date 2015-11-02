d198ae3f55f75b44689972f6e89bb1f142f88608
edwin.webb <edwin.webb@gmail.com>
2015-11-02 10:57:32 +0000

    Add missing file

---
+++ templates/generic.js
@@ -0,0 +1,18 @@
+/**
+ * {%=o.description%}
+ *
+ * @exports {%=o.exports%}
+ */
+import THREE from 'three';
+
+export default class {%=o.exports%} {
+
+  constructor(options) {
+    
+  }
+  {% for(var i = 0; i < o.functions.length; i++) { %}
+  {%= o.functions[i] %}() {
+
+  }
+  {% } %}
+}
