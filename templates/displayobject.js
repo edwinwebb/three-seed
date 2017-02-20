180477c3f591b335b0212f6475509c3874415c39
Edwin Webb <edwin.webb@gmail.com>
2017-02-20 13:20:56 +0000

    update reactman

--- templates/displayobject.js
+++ templates/displayobject.js
@@ -1,12 +1,12 @@
 /**
  * @exports {%=o.exports%}
  */
-import THREE from 'three';
+import { Group } from 'three';
 
-export default class {%=o.exports%} {
+export default class {%=o.exports%} extends Group {
 
-  constructor(options) {
-
+  constructor(...args) {
+    super(...args);
   }
   {% for(var i = 0; i < o.functions.length; i++) { %}
   {%= o.functions[i] %}() {
