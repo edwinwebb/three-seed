6cb72a8700812d007e72545ec42ab640b953c3e3
Edwin Webb <edwin.webb@gmail.com>
2017-02-21 12:55:58 +0000

    Save point, test std

--- app/Renderer/EffectRenderer.js
+++ app/Renderer/EffectRenderer.js
@@ -12,11 +12,12 @@ export default class EffectRenderer {
     this.renderer = new WebGLRenderer(...args);
     this.resolution = window.devicePixelRatio;
     this.animationToken = 0;
-    this.rTarget1 = RenderTarget.clone();
+    this.rTarget1 = RenderTarget;
     this.rTarget2 = RenderTarget.clone();
     this.writeBuffer = this.rTarget1;
     this.readBuffer = this.rTarget2;
-    this.passes = [new ShaderPass(CopyShader)];
+    this.passes = [];
+    // this.copyPass = new ShaderPass(CopyShader);
     this.camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
     this.quad = new Mesh(new PlaneGeometry(2,2), null);
     this.scene = new Scene();
@@ -31,6 +32,13 @@ export default class EffectRenderer {
     this.setStore();
 
     this.resizeHandler();
+
+    // this.passes.push(new RenderPass());
+    // this.addPass(CopyShader);
+  }
+
+  addPass(shader) {
+    this.passes.push(shader);
   }
 
   setStore() {
@@ -40,18 +48,18 @@ export default class EffectRenderer {
   }
 
   resizeHandler() {
-    const { width, height } = this.getWindowSize();
+    // const { width, height } = this.getWindowSize();
 
-    this.renderer.setSize(width, height);
+    // this.renderer.setSize(width, height);
 
-    if (this.camera) {
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-    }
+    // if (this.camera) {
+    //   this.camera.aspect = width / height;
+    //   this.camera.updateProjectionMatrix();
+    // }
 
-    this.setStore();
+    // this.setStore();
 
-    RendererStore.emitChange();
+    // RendererStore.emitChange();
   }
 
   getWindowSize() {
@@ -87,14 +95,15 @@ export default class EffectRenderer {
     this.writeBuffer = this.rTarget1;
     this.readBuffer = this.rTarget2;
 
-    //this.renderer.render(this.scene, this.camera);
-
     this.passes.forEach( (pass)=>{
       pass.enabled && pass.render(this.renderer, this.writeBuffer, this.readBuffer, {
         camera: this.camera,
         scene: this.scene,
         quad: this.quad
       });
+      const tmp = this.readBuffer;
+      this.readBuffer = this.writeBuffer;
+      this.writeBuffer = tmp;
     } );
   }
 
@@ -108,18 +117,18 @@ export class ShaderPass {
     this.textureID = ID;
     this.uniforms = UniformsUtils.clone(uniforms);
     this.material = new ShaderMaterial({
-      uniforms,
+      uniforms: this.uniforms,
       vertexShader,
       fragmentShader
     });
-    this.renderToScreen = true;
+    this.renderToScreen = false;
     this.enabled = true;
     this.needsSwap = true;
     this.clear = false;
   }
 
-  render(renderer, writeBuffer, readBuffer, EffectComposer) {
-    const { camera, scene, quad } = EffectComposer;
+  render(renderer, writeBuffer, readBuffer, RendererBits) {
+    const { camera, scene, quad } = RendererBits;
     if (this.uniforms[this.textureID]) {
       this.uniforms[this.textureID].value = readBuffer.texture
     }
@@ -152,3 +161,18 @@ export const CopyShader = {
       gl_FragColor = opacity * texel;
     }`
 }
+
+export class RenderPass {
+  constructor(scene, camera) {
+    this.scene = scene;
+    this.camera = camera;
+    this.enabled = true;
+    this.clear = true;
+    this.needsSwap = false;
+  }
+  render(renderer, writeBuffer, readBuffer) {
+    renderer.render(this.scene, this.camera, readBuffer, true);
+  }
+}
+
+
--- app/app.js
+++ app/app.js
@@ -6,37 +6,40 @@
  *
  */
 
-import Renderer from './Renderer/EffectRenderer';
+import Renderer from './Renderer/Renderer';
 // import Composer from './Composer/Composer';
 import { Scene, PerspectiveCamera } from 'three';
 import * as THREE from 'three'; // used for Orbit Controls
 import TestCube from './objects/TestCube';
-// import { RenderPass, ShaderPass } from 'three-effectcomposer-es6';
-// import { FXAAShader } from './Shaders/fxaa/fxaa';
-// import { Grain } from './Shaders/grain/grain';
+import { ShaderPass, RenderPass, CopyShader } from './Renderer/EffectRenderer';
+import { FXAAShader } from './Shaders/fxaa/fxaa';
+import { Grain } from './Shaders/grain/grain';
 
 const renderer = new Renderer({antialias: false, alpha: false});
 const scene = new Scene();
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-const OrbitControls = require('three-orbit-controls')(THREE)
+//const OrbitControls = require('three-orbit-controls')(THREE)
 const mesh = new TestCube();
 // const composer = new Composer(renderer);
-// const FXAA = new ShaderPass(FXAAShader);
-// const grain = new ShaderPass(Grain);
+const rPass = new RenderPass(scene, camera);
+const FXAA = new ShaderPass(FXAAShader);
+const grain = new ShaderPass(Grain);
+const x = new ShaderPass(CopyShader);
 
-new OrbitControls(camera);
+// new OrbitControls(camera);
 
 scene.add(mesh);
-camera.position.z = 100;
-
-renderer.camera = camera;
-renderer.scene = scene;
+camera.position.z = -100;
+camera.lookAt( mesh.position );
 
 // FXAA.renderToScreen = true;
-// grain.renderToScreen = true;
-// composer.addPass(new RenderPass(scene, camera));
-// composer.addPass(FXAA);
-// //composer.addPass(grain);
+// x.renderToScreen = true;
+// // grain.renderToScreen = true;
+// // composer.addPass(new RenderPass(scene, camera));
+// renderer.addPass(rPass);
+// //renderer.addPass(x);
+// renderer.addPass(FXAA);
+//renderer.addPass(grain);
 
 // window.composer = composer;
 
@@ -44,4 +47,4 @@ renderer.start();
 
 document.body.style.margin = 0;
 document.body.style.overflow = 'hidden';
-document.body.appendChild( renderer.renderer.domElement );
+document.body.appendChild( renderer.domElement );
