ab0641b70c274a123b1ba13aea3a7eb012a3ea69
Edwin Webb <edwin.webb@gmail.com>
2017-02-21 10:09:45 +0000

    Add a new file to handle GLSL filters

---
+++ app/Renderer/EffectRenderer.js
@@ -0,0 +1,153 @@
+import { WebGLRenderer, WebGLRenderTarget, LinearFilter, RGBFormat, OrthographicCamera, Mesh, Scene, PlaneGeometry } from 'three';
+import RendererStore from '../stores/RendererStore.js';
+import AnimationStore from '../stores/AnimationStore.js';
+
+export default class EffectRenderer {
+
+  constructor(...args) {
+
+    const { width, height } = this.getWindowSize();
+    const RenderTarget = new WebGLRenderTarget(width, height, { minFilter: LinearFilter, magFilter: LinearFilter, format: RGBFormat, stencilBuffer: false });
+
+    this.renderer = new WebGLRenderer(...args);
+    this.resolution = window.devicePixelRatio;
+    this.animationToken = 0;
+    this.rTarget1 = RenderTarget.clone();
+    this.rTarget2 = RenderTarget.clone();
+    this.copyPass = new ShaderPass(CopyShader);
+    this.writeBuffer = this.rTarget1;
+    this.readBuffer = this.rTarget2;
+    this.passes = [];
+    this.camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    this.quad = new Mesh(new PlaneGeometry(2,2), null);
+    this.scene = new Scene();
+    this.scene.add(this.quad);
+
+    window.addEventListener('resize', this.resizeHandler.bind(this));
+
+    RendererStore.set('resolution', this.resolution);
+
+    this.renderer.setPixelRatio(this.resolution);
+
+    this.setStore();
+
+    this.resizeHandler();
+  }
+
+  setStore() {
+    const { width, height } = this.getWindowSize();
+    RendererStore.set('width', width);
+    RendererStore.set('height', height);
+  }
+
+  resizeHandler() {
+    const { width, height } = this.getWindowSize();
+
+    this.renderer.setSize(width, height);
+
+    if (this.camera) {
+      this.camera.aspect = width / height;
+      this.camera.updateProjectionMatrix();
+    }
+
+    this.setStore();
+
+    RendererStore.emitChange();
+  }
+
+  getWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    return { width, height };
+  }
+
+  start() {
+    this.active = true;
+    this.animationToken = window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  stop() {
+    window.cancelAnimationFrame(this.animationToken);
+    this.active = false;
+  }
+
+  isRenderable() {
+    return this.scene !== false && this.camera !== false;
+  }
+
+  animate() {
+    if (this.active && this.isRenderable()) {
+      this.render(this.scene, this.camera);
+      AnimationStore.emitChange();
+      window.requestAnimationFrame(this.animate.bind(this));
+    }
+  }
+
+  render() {
+    this.writeBuffer = this.rTarget1;
+    this.readBuffer = this.rTarget2;
+
+    this.passes.forEach( (pass)=>{
+      pass.enabled && pass.render(this.renderer, this.writeBuffer, this.readBuffer, {
+        camera: this.camera,
+        scene: this.scene,
+        quad: this.quad
+      });
+    } );
+  }
+
+}
+
+import { UniformsUtils, ShaderMaterial } from 'three'
+
+export class ShaderPass {
+  constructor(shader, ID = 'tDiffuse') {
+    const { uniforms, vertexShader, fragmentShader } = shader;
+    this.textureID = ID;
+    this.uniforms = UniformsUtils.clone(uniforms);
+    this.material = new ShaderMaterial({
+      uniforms,
+      vertexShader,
+      fragmentShader
+    });
+    this.renderToScreen = false;
+    this.enabled = true;
+    this.needsSwap = true;
+    this.clear = false;
+  }
+
+  render(renderer, writeBuffer, readBuffer, EffectComposer) {
+    const { camera, scene, quad } = EffectComposer;
+    if (this.uniforms[this.textureID]) {
+      this.uniforms[this.textureID].value = readBuffer.texture
+    }
+
+    quad.material = this.material;
+
+    if (this.renderToScreen) {
+      renderer.render(scene, camera);
+    } else {
+      renderer.render(scene, camera, writeBuffer, this.clear);
+    }
+  }
+}
+
+export const CopyShader = {
+  uniforms: {
+    'tDiffuse': { type: 't', value: null },
+    'opacity': { type: 'f', value: 1.0 }
+  },
+  vertexShader: `varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }`,
+  fragmentShader: `uniform float opacity;
+    uniform sampler2D tDiffuse;
+    varying vec2 vUv;
+    void main() {
+      vec4 texel = texture2D( tDiffuse, vUv );
+      gl_FragColor = opacity * texel;
+    }`
+}
--- app/app.js
+++ app/app.js
@@ -6,23 +6,23 @@
  *
  */
 
-import Renderer from './Renderer/Renderer';
-import Composer from './Composer/Composer';
+import Renderer from './Renderer/EffectRenderer';
+// import Composer from './Composer/Composer';
 import { Scene, PerspectiveCamera } from 'three';
 import * as THREE from 'three'; // used for Orbit Controls
 import TestCube from './objects/TestCube';
-import { RenderPass, ShaderPass } from 'three-effectcomposer-es6';
-import { FXAAShader } from './Shaders/fxaa/fxaa';
-import { Grain } from './Shaders/grain/grain';
+// import { RenderPass, ShaderPass } from 'three-effectcomposer-es6';
+// import { FXAAShader } from './Shaders/fxaa/fxaa';
+// import { Grain } from './Shaders/grain/grain';
 
-const renderer = new Renderer({antialias: false, alpha: true});
+const renderer = new Renderer({antialias: false, alpha: false});
 const scene = new Scene();
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
 const OrbitControls = require('three-orbit-controls')(THREE)
 const mesh = new TestCube();
-const composer = new Composer(renderer);
-const FXAA = new ShaderPass(FXAAShader);
-const grain = new ShaderPass(Grain);
+// const composer = new Composer(renderer);
+// const FXAA = new ShaderPass(FXAAShader);
+// const grain = new ShaderPass(Grain);
 
 new OrbitControls(camera);
 
@@ -32,16 +32,16 @@ camera.position.z = 100;
 renderer.camera = camera;
 renderer.scene = scene;
 
-FXAA.renderToScreen = true;
-grain.renderToScreen = true;
-composer.addPass(new RenderPass(scene, camera));
-composer.addPass(FXAA);
-//composer.addPass(grain);
+// FXAA.renderToScreen = true;
+// grain.renderToScreen = true;
+// composer.addPass(new RenderPass(scene, camera));
+// composer.addPass(FXAA);
+// //composer.addPass(grain);
 
-window.composer = composer;
+// window.composer = composer;
 
 renderer.start();
 
 document.body.style.margin = 0;
 document.body.style.overflow = 'hidden';
-document.body.appendChild( renderer.domElement );
+document.body.appendChild( renderer.renderer.domElement );
