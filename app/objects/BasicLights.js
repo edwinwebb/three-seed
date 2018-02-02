06193bb9c20a864774e1047ee5dec30759dd1e05
edwin.webb <edwin.webb@gmail.com>
2018-02-01 17:59:31 -0800

    lights

--- app/objects/BasicLights.js
+++ app/objects/BasicLights.js
@@ -6,9 +6,9 @@ export default class BasicLights extends Group {
     super(...args);
 
     const point = new PointLight(0xFFFFFF, 1, 10, 1);
-    const dir = new SpotLight(0xFFFFFF, 0.8, 7, 1.15, 1, 1);
-    const ambi = new AmbientLight( 0x404040 , 0.66); // soft white light
-    const hemi = new HemisphereLight( 0xffffbb, 0x080820, 0.8 )
+    const dir = new SpotLight(0xFFFFFF, 0.8, 7, 0.8, 1, 1);
+    const ambi = new AmbientLight( 0x404040 , 0.66);
+    const hemi = new HemisphereLight( 0xffffbb, 0x080820, 1.15 )
 
     dir.position.set(5, 1, 2);
     dir.target.position.set(0,0,0);
