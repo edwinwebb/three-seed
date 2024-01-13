41d9cc7dbe3b5868621c76fb5b7e1242eb3367ae
andres <andres_felipe1822@hotmail.com>
2024-01-12 23:27:07 -0500

    v0.0.4

--- src/objects/Pie/Pie.js
+++ src/objects/Pie/Pie.js
@@ -20,29 +20,9 @@ export default class Pie extends Group {
     const objeto1 = gltf.scene.getObjectByName('01');
 
     if (objeto1){
-      //objeto1.position.set(0,,0);
-      // var valorEnY=40;
-      // var total = valorEnY*2
-      // objeto1.scale.y = total;
-      // if (total > 10){
-      //   objeto1.material.color.set(0xffffff);
-      // };
-      
-         // objeto1.position.set(0, 0, 0);
       var valorEnY = 40;
       var total = valorEnY * 2;
-    
-
-     if (total > 10) {
-        // Cambiar el color del material del objeto
-        objeto1.material.color.set(0xf0ffff);
-        objeto1.scale.y = total;
-      };
-      
-      
-
-      
-      
+      objeto1.scale.y = total;
     };
 
     
